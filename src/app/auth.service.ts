34ead208e7efe9c534da5bfd004a4501b5ddd5aa
birametgod <birametgod95@gmail.com>
2018-12-06 10:03:49 +0100

    ajouter message si identifiant incorrecte

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -27,7 +27,7 @@ export class AuthService {
               console.log(this.user);
               resolve(true);
             }
-          })
+          });
           console.log(this.isAuth);
         },
         (error) => {
